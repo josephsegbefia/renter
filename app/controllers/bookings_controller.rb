1c9fda0d5bdd0c7ec7ddc51100264c219fcd2302
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-12-05 15:25:38 +0000

    Start booking views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+    @shop = Shop.find(params[:id])
+    @unit = Unit.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @shop = Shop.find(params[:id])
+    @unit = Unit.find(params[:id])
+    @booking.user = current_user
+    @booking.shop_id = @shop.id
+    @booking.unit_id = @unit.id
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
