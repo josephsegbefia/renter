4d731446cdd7d49a7a7ee149e36df2bf869b3ee6
ugorji eke <ugorjiekechibuzo@gmail.com>
2023-12-05 13:16:21 +0100

    Created the shop new and create, Implemented Pundit

---
+++ app/policies/shop_policy.rb
@@ -0,0 +1,15 @@
+class ShopPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+    def new
+      create?
+    end
+
+    def create?
+      true
+    end
+  end
+end
