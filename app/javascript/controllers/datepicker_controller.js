cea06bd945348b2cb495b3f60537cf6676d23508
Christian Davidsson davidssonchristian07@gmail.com <davidssonchristian07@gmail.com>
2023-12-07 00:29:03 +0100

    fixed the flatpicker to show the calendar

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-
+import flatpickr from "flatpickr";
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
