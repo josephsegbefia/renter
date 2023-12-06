a5fdd570ec6c8e1c59a57a0a23482ac7441dc484
Christian Davidsson davidssonchristian07@gmail.com <davidssonchristian07@gmail.com>
2023-12-06 12:21:52 +0100

    corrected route typo

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "dashboard", to: "dashboard#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :shops only: [:new, :create, :index, :show] do
+  resources :shops, only: [:new, :create, :index, :show] do
     resources :units, only: [:index, :new, :create] do
       resources :bookings, only: [:new, :create]
     end
