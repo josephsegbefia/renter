69783ed218b4e1b36ec17b3d077a0aec3b853a5c
Christian Davidsson davidssonchristian07@gmail.com <davidssonchristian07@gmail.com>
2023-12-07 15:08:30 +0100

    added multiple units for every shop, created 4 shops

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_06_181910) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_07_132533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,6 +95,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_06_181910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "shop_id", null: false
+    t.string "title"
     t.index ["shop_id"], name: "index_units_on_shop_id"
   end
 
