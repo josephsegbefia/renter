a55de2c87dd79c1eccfd93548cdf710c0131944d
Christian Davidsson davidssonchristian07@gmail.com <davidssonchristian07@gmail.com>
2023-12-11 15:25:49 +0100

    added calendar and availability logic, + fixed the links of the Unit Show page

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ gem "simple_form", github: "heartcombo/simple_form"
 gem "sassc-rails"
 gem "pundit"
 gem "cloudinary"
+gem "simple_calendar", "~> 2.4"
 
 group :development, :test do
   gem "dotenv-rails"
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,4 +2,17 @@ class Unit < ApplicationRecord
   belongs_to :shop
   has_many :bookings
   has_one_attached :photo
+
+  def self.available(period)
+    bookings = Booking.overlap(period)
+    where.not(id: bookings.select(:unit_id))
+  end
+
+  def available?(period)
+    if bookings.loaded?
+      bookings.none? { |booking| booking.overlap?(period) }
+    else
+      bookings.overlap(period).none?
+    end
+  end
 end
